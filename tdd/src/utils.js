df844a3b054af17eac6211908582464efb7d0e73
RedLincoln <miguel_lcp@hotmail.com>
2019-10-12 23:28:44 +0100

    green: seconds input must be a string

--- tdd/src/utils.js
+++ tdd/src/utils.js
@@ -1,6 +1,6 @@
 
 function clockFormatToSeconds(time) {
-	if (time.length >= 3){
+	if (time.length >= 3 || typeof time !== 'string'){
 		throw new Error('Bad input');
 	}
 	var value = parseInt(time);
--- tdd/test/utils.test.js
+++ tdd/test/utils.test.js
@@ -1,28 +1,46 @@
 var clockFormatToSeconds = require('../src/utils.js');
 
 test('seconds units must be well calculated', () => {
- 	var oneSecond = "1";
+ 	var input = '1';
 
- 	var result = clockFormatToSeconds(oneSecond);
+ 	var result = clockFormatToSeconds(input);
 
  	expect(result).toBe(1);
 });
 
 
 test('seconds tens must be well calculated', () => {
- 	var oneSecond = "10";
+ 	var input = '10';
 
- 	var result = clockFormatToSeconds(oneSecond);
+ 	var result = clockFormatToSeconds(input);
 
  	expect(result).toBe(10);
 });
 
 
-test('seconds hundreds or more most couse an Error', () => {
- 	var oneSecond = "100";
+test('seconds hundreds or more must couse an Error', () => {
+ 	var input = '100';
 
  	expect(() => {
- 		clockFormatToSeconds(oneSecond);
+ 		clockFormatToSeconds(input);
  	}).toThrow('Bad input');
 });
+
+test('seconds input must be a string', () => {
+ 	var input = true;
+
+ 	expect(() => {
+ 		clockFormatToSeconds(input);
+ 	}).toThrow('Bad input');
+});
+
+test('seconds input must contain only integer(s)', () => {
+ 	var input = 'tr';
+
+ 	expect(() => {
+ 		clockFormatToSeconds(input);
+ 	}).toThrow('Bad input');
+});
+
+
 
