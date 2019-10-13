fe57cd0a808879e817f9134069ff50332f92f45b
RedLincoln <miguel_lcp@hotmail.com>
2019-10-13 11:20:41 +0100

    Refactor: test to describe it format

--- tdd/test/digitalClockToSeconds.test.js
+++ tdd/test/digitalClockToSeconds.test.js
@@ -1,122 +1,128 @@
 var digitalClockToSeconds = require('../src/utils.js');
 
-test('field units must be well calculated', () => {
- 	var input = '1';
 
- 	var result = digitalClockToSeconds(input);
+describe('digitalClockToSeconds must throw an Error when', () => {
+	
+	test('fields separator is not a column', () => {
+	 	var input = '0,59';
 
- 	expect(result).toBe(1);
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('input have more than two fields', ()=>{
+		var input = '0:0:50';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+
+	test('second field is empty', ()=>{
+		var input = '1:';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('minutes field is empty with separator', ()=>{
+		var input = ':1';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('the length of the field is greater than two caracters', () => {
+	 	var input = '100';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('field input is not a string', () => {
+	 	var input = true;
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('field is not a Number', () => {
+	 	var input = '4r';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+
+	test('input is empty', () => {
+	 	var input = '';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+
+	test('input only have spaces', () => {
+	 	var input = '  ';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('field is negative', () => {
+	 	var input = '-1';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+	test('field is greater than 60', () => {
+	 	var input = '60';
+
+	 	expect(() => {
+	 		digitalClockToSeconds(input);
+	 	}).toThrow('Bad input');
+	});
+
+});
+
+describe("digitalClockToSeconds must calculate the right value when", () => {
+
+	test('field is a unit integer', () => {
+	 	var input = '1';
+
+	 	var result = digitalClockToSeconds(input);
+
+	 	expect(result).toBe(1);
+	});
+
+	test('field is a tens integer', () => {
+	 	var input = '10';
+
+	 	var result = digitalClockToSeconds(input);
+
+	 	expect(result).toBe(10);
+	});
+
+	test('minutes:seconds have proper format', () => {
+	 	var input = '2:59';
+
+	 	var result = digitalClockToSeconds(input);
+	 	
+	 	expect(result).toBe(179);
+	});
+
 });
 
 
-test('field tens must be well calculated', () => {
- 	var input = '10';
-
- 	var result = digitalClockToSeconds(input);
-
- 	expect(result).toBe(10);
-});
-
-
-test('field hundreds or more must couse an Error', () => {
- 	var input = '100';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('field input must be a string', () => {
- 	var input = true;
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('field input must contain only integer(s)', () => {
- 	var input = '4r';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-
-test('field input empty must couse an Error', () => {
- 	var input = '';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-
-test('field input spaces must couse an Error', () => {
- 	var input = '  ';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('field input must must be positive', () => {
- 	var input = '-1';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('seconds input must must be lewer to 60', () => {
- 	var input = '60';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('minutes:seconds must be well calculated', () => {
- 	var input = '2:59';
-
- 	var result = digitalClockToSeconds(input);
- 	
- 	expect(result).toBe(179);
-});
-
-
-test('minutes:seconds seperator must be a column', () => {
- 	var input = '0,59';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-test('must throw an Error wiht three or more fields', ()=>{
-	var input = '0:0:50';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-
-test('second empty must throw an Error', ()=>{
-	var input = '1:';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
-
-
-
-test('minutes empty must throw an Error', ()=>{
-	var input = ':1';
-
- 	expect(() => {
- 		digitalClockToSeconds(input);
- 	}).toThrow('Bad input');
-});
 
